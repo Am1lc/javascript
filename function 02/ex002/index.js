b1fac018bc2edc8e5c222dbe49a47ee2891f8243
Am1lc <142388129+Am1lc@users.noreply.github.com>
2023-12-10 23:47:18 +0200

    boolean

---
+++ function 02/ex002/index.js
@@ -0,0 +1,11 @@
+// Equality Operator
+
+function testEqual(val) {
+    if (val == 12) {
+        return "Equal";
+    }
+    return "Not Equal";
+}
+
+// change this value to test
+console.log(testEqual(10));
