93d7fc1c7bd0399c617877caaac5f262b14f4c3a
Am1lc <142388129+Am1lc@users.noreply.github.com>
2023-12-11 17:50:14 +0200

    boolean

--- function 02/ex002/index.js
+++ function 02/ex002/index.js
@@ -9,3 +9,28 @@ function testEqual(val) {
 
 // change this value to test
 console.log(testEqual(10));
+
+
+function testStrict(val) {
+    if (val === 7) {
+        return "Equal";
+    }
+    return "Not Equal";
+}
+
+// change this value to test
+console.log(testStrict(7));
+
+/*
+3 == 3
+3 === '3'
+*/
+
+function compareEquality(a,b) {
+    if (a === b) {
+        return "Equal";
+    }
+    return "Not Equal";
+}
+
+console.log(compareEquality(10, "10"));
