cd7783d75a7fc50553c81f104a0d1fa7728a1323
Am1lc <142388129+Am1lc@users.noreply.github.com>
2023-12-12 21:14:39 +0200

    record collection

---
+++ objects/ex012/index.js
@@ -0,0 +1,22 @@
+var myPlants = [
+    {
+        type: "flowers",
+        list: [
+            "rose",
+            "tulip",
+            "dandelion"
+        ]
+    },
+    {
+        type: "trees",
+        list: [
+            "fir",
+            "pine",
+            "birch"
+        ]
+    }
+];
+
+var secondTree = myPlants[1].type;
+
+console.log(secondTree);
