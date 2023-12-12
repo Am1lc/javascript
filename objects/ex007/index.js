7bfbdb6e87a7835b334e68b6afb35a7fdec9214a
Am1lc <142388129+Am1lc@users.noreply.github.com>
2023-12-12 14:21:52 +0200

    objects

---
+++ objects/ex007/index.js
@@ -0,0 +1,24 @@
+// Delete Properties from an Object
+
+var ourDog = {
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything!"],
+    "bark": "bow-wow"
+};
+
+delete ourDog.bark;
+
+var myDog = {
+    "name": "Happy coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+};
+
+delete myDog.tails
+
+console.log(ourDog);
+console.log(myDog);
---
+++ objects/ex009/index.js
@@ -0,0 +1,17 @@
+// Testing Objects for Properties
+
+var myObj = {
+    gift: "pony",
+    pet: "kitten",
+    bed: "sleigh"
+};
+
+function checkObj(checkProp) {
+    if (myObj.hasOwnProperty(checkProp)) {
+        return myObj[checkProp];
+    } else {
+        return "Not Found"
+    }
+}
+
+console.log(checkObj("bed"));
