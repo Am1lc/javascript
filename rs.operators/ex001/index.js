5f0eb2a57836698d0adec88520689bee24ff8dde
Am1lc <142388129+Am1lc@users.noreply.github.com>
2023-12-15 22:43:00 +0200

    profile lookup

---
+++ rs.operators/ex001/index.js
@@ -0,0 +1,42 @@
+// Profile Lookup
+
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["Javascript", "Gaming", "Foxes"]
+    },
+];
+
+function lookUpProfile(name, prop){
+    for (var i = 0; i < contacts.length; i++){
+        if(contacts[i].firstName === name) {
+            return contacts[i][prop] || "No such property";
+        }
+    }
+    return "No such contact";
+}
+
+// change these values to test your function
+var data = lookUpProfile("Akira", "likes")
+
+console.log(data);
