bad9ad96f0f08ceba9a296276a8446475c5d20f9
Am1lc <142388129+Am1lc@users.noreply.github.com>
2023-12-18 15:31:33 +0200

    destructuring

--- rs03.function/ex003/index.js
+++ rs03.function/ex003/index.js
@@ -10,10 +10,18 @@ console.log(increment(5));
 
 // Use the Rest Operator with Function Parameters
 
-const sum = (function() {
+/*const sum = (function() {
     return function sum(x, y, z) {
         const args = [x, y, z];
         return args.reduce((a, b) => a + b, 0);
     };
 }) ();
-console.log(sum(1, 2, 3))
+console.log(sum(1, 2, 3))*/
+
+/*the code below is a shortened version of the code above, Rest operator*/
+const sum = (function() {
+    return function sum(...args) {
+        return args.reduce((a, b) => a + b, 0);
+    };
+}) ();
+console.log(sum(1, 2, 3, 4))
