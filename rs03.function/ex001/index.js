7eca0ed87501c64b4c8c413d0310829bcda1f385
Am1lc <142388129+Am1lc@users.noreply.github.com>
2023-12-18 14:04:40 +0200

    arrow

---
+++ rs03.function/ex001/index.js
@@ -0,0 +1,8 @@
+// Use Arrow Functions to Write Concise Anonymous funtions
+
+ /*var magic = function() {
+    return new Date();
+};*/
+
+/* the code below is a shortened version of the code above */
+var magic = () => new Date();
---
+++ rs03.function/ex003/index.js
@@ -0,0 +1,19 @@
+// Default Parameters
+const increment = (function() {
+    return function increment(number, value = 1) {
+        return number + value;
+    };
+}) ();
+
+console.log(increment(5, 2));
+console.log(increment(5));
+
+// Use the Rest Operator with Function Parameters
+
+const sum = (function() {
+    return function sum(x, y, z) {
+        const args = [x, y, z];
+        return args.reduce((a, b) => a + b, 0);
+    };
+}) ();
+console.log(sum(1, 2, 3))
